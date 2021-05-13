aa00cfdc0c7d04500fb54747249b6f245f3b741c
Oleksandr Mazurenko <oleksandrmazurenko@yahoo.com>
2021-05-13 21:22:34 +0300

    commit

--- dz_18_blank/unit_18/unit_18.js
+++ dz_18_blank/unit_18/unit_18.js
@@ -65,22 +65,14 @@ document.querySelector('.b-4').onclick = t4;
 /*  На странице созданы 3 p.task-5 c атрибутом data. С помощью forEach переберите их и добавьте событие клик. Напишите функцию t5, которая будет запускаться при клике и добавлять атрибут data элемента, по которому кликнули в массив a5_res.*/
 
 function t5() {
-    let a = document.querySelectorAll('p.task-5');
-    
-    for(let i = 0; i < a.length; i++) {
-        if(a[i].onclick){
-            a5_res.push(a[0].getAttribute('data'));
-        }
-    }
-
+    a5_res.push(this.getAttribute('data'));
+  
     document.querySelector('.out-5').innerHTML = a5_res;
 }
 
 document.querySelectorAll('.task-5').forEach(elem => {
     elem.onclick = t5;
 });
-
-
 
 // Task 6 ============================================
 /*  Дана строка str6='helloworld' - преобразуйте ее в массив и присвойте a6_res. Выведите на страницу. Запускаться решение должно при вызове функции t6. */
